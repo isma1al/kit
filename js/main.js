ad6d93f4a4f8fc4233579fa6ecfdbfa4e5eb36ed
isma1al <88884006+isma1al@users.noreply.github.com>
2022-11-13 15:48:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ new Swiper('.products__img', {
       },
 
       slidesPerView: 'auto',
+      loop: true,
 });
+
+
+new Swiper('.works__item-img', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
